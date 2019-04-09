1674b9d11187b8a78b65bddb999e3f1b7759c20c
Divya Nair <divnairs@gmail.com>
2019-04-09 14:48:02 -0700

    add new todo to local state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 const App = () => {
-  return <div className="app">hi</div>
+  const [newTodo, setNewTodo] = useState('')
+  const [todoArr, setTodoArr] = useState([])
+
+  const changeNewTodo = e => setNewTodo(e.target.value)
+
+  const submitNewTodo = e => {
+    e.preventDefault()
+    if (!newTodo) return
+    setTodoArr([...todoArr, { todo: newTodo, done: false }])
+    setNewTodo('')
+  }
+
+  return (
+    <div className="app">
+      <form onSubmit={submitNewTodo}>
+        <input
+          onChange={changeNewTodo}
+          value={newTodo}
+          type="text"
+          placeholder="+ New Todo"
+        />
+      </form>
+    </div>
+  )
 }
 
 export default App
