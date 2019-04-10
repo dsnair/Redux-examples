c8c21603462215099424af3d2865e88c3ab1ec23
Divya Nair <divnairs@gmail.com>
2019-04-09 18:41:11 -0700

    refactor from useState to useReducer

--- src/App.js
+++ src/App.js
@@ -1,50 +1,87 @@
-import React, { useState } from 'react'
+import React, { useReducer } from 'react'
 import './App.css'
 
-const App = () => {
-  const [newTodo, setNewTodo] = useState('')
-  const [todoArr, setTodoArr] = useState([])
+const initialState = {
+  newTodo: '',
+  todoArr: []
+}
 
-  const changeNewTodo = e => setNewTodo(e.target.value)
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'NEW_TODO_ADDED':
+      return {
+        ...state,
+        newTodo: action.newTodo
+      }
+
+    case 'NEW_TODO_SUBMITTED':
+      if (!state.newTodo) return state
+      return {
+        ...state,
+        newTodo: '',
+        todoArr: [
+          ...state.todoArr,
+          { todo: state.newTodo, done: false, id: state.todoArr.length }
+        ]
+      }
+
+    case 'DONE_TOGGLED':
+      const newTodoArr = [...state.todoArr]
+      newTodoArr.splice(action.id, 1, {
+        ...state.todoArr[action.id],
+        done: !state.todoArr[action.id].done
+      })
+      return {
+        ...state,
+        todoArr: newTodoArr
+      }
+
+    default:
+      return state
+  }
+}
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  const addNewTodo = e =>
+    dispatch({ type: 'NEW_TODO_ADDED', newTodo: e.target.value })
 
   const submitNewTodo = e => {
     e.preventDefault()
-    if (!newTodo) return
-    setTodoArr([...todoArr, { todo: newTodo, done: false, id: todoArr.length }])
-    setNewTodo('')
-  }
-
-  const toggleDone = id => {
-    const newTodoArr = [...todoArr]
-    newTodoArr.splice(id, 1, {
-      ...todoArr[id],
-      done: !todoArr[id].done
-    })
-    setTodoArr(newTodoArr)
+    dispatch({ type: 'NEW_TODO_SUBMITTED' })
   }
 
   return (
     <div className="app">
-      {todoArr.map(obj => (
+      {state.todoArr.map(obj => (
         <div className="todoArr" key={obj.id}>
           <input
             value={obj.todo}
             style={
               obj.done
-                ? { textDecoration: 'line-through', textDecorationColor: 'orangered' }
+                ? {
+                    textDecoration: 'line-through',
+                    textDecorationColor: 'orangered',
+                    textDecorationStyle: 'wavy'
+                  }
                 : { textDecoration: 'none' }
             }
             type="text"
             readOnly
           />
-          <button onClick={() => toggleDone(obj.id)}>&#10003;</button>
+          <button
+            onClick={() => dispatch({ type: 'DONE_TOGGLED', id: obj.id })}
+          >
+            &#10003;
+          </button>
         </div>
       ))}
 
       <form onSubmit={submitNewTodo}>
         <input
-          onChange={changeNewTodo}
-          value={newTodo}
+          onChange={addNewTodo}
+          value={state.newTodo}
           type="text"
           placeholder="&#x2B; New Todo"
         />
